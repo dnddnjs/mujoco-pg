2f6afbb8056c3aabc54ff9ad5b971f684508acf9
dnddnjs <dnddnjs11@naver.com>
2018-06-26 23:02:55 +0900

    edit model. add batch norm layer

--- model.py
+++ model.py
@@ -1,32 +1,41 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from hparams import HyperParams as hp
 
 
 class Actor(nn.Module):
     def __init__(self, num_inputs, num_outputs):
         super(Actor, self).__init__()
-        self.fc1 = nn.Linear(num_inputs, 64)
-        self.fc2 = nn.Linear(64, 64)
-        self.fc3 = nn.Linear(64, num_outputs)
+        self.bn0 = nn.BatchNorm1d(num_inputs)
+        self.fc1 = nn.Linear(num_inputs, hp.hidden)
+        self.fc2 = nn.Linear(hp.hidden, hp.hidden)
+        self.fc3 = nn.Linear(hp.hidden, num_outputs)
+        self.fc3.weight.data.mul_(0.1)
+        self.fc3.bias.data.mul_(0.0)
 
     def forward(self, x):
+        x = self.bn0(x)
         x = F.tanh(self.fc1(x))
         x = F.tanh(self.fc2(x))
         mu = self.fc3(x)
         logstd = torch.zeros_like(mu)
         std = torch.exp(logstd)
-        return mu, std
+        return mu, std, logstd
 
 
 class Critic(nn.Module):
     def __init__(self, num_inputs):
         super(Critic, self).__init__()
-        self.fc1 = nn.Linear(num_inputs, 64)
-        self.fc2 = nn.Linear(64, 64)
-        self.fc3 = nn.Linear(64, 1)
+        self.bn0 = nn.BatchNorm1d(num_inputs)
+        self.fc1 = nn.Linear(num_inputs, hp.hidden)
+        self.fc2 = nn.Linear(hp.hidden, hp.hidden)
+        self.fc3 = nn.Linear(hp.hidden, 1)
+        self.fc3.weight.data.mul_(0.1)
+        self.fc3.bias.data.mul_(0.0)
 
     def forward(self, x):
+        x = self.bn0(x)
         x = F.tanh(self.fc1(x))
         x = F.tanh(self.fc2(x))
         v = self.fc3(x)
